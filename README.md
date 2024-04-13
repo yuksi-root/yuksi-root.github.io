779c6bc04e338cadb95fdf9934302387d210ce0b
yuksi-root <yukselcan1990@gmail.com>
2024-04-13 15:40:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# https://yuksel-root.github.io/
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+function Home() {
+  return (
+    <section >
+       
+       <a href="https://yuksi-root.github.io/">https://yuksi-root.github.io/</a>
+
+    </section>
+  );
+}
+
+export default Home;
